7338bd261b470e0cff9d4a966aa0dcc277bf9890
hackticubrube <109615015+hackticubrube@users.noreply.github.com>
2022-10-14 12:09:18 +0300

    bb
    bb

---
+++ config/config.php
@@ -0,0 +1,8 @@
+<?php
+define("DB_HOST", "127.0.0.1");
+define("DB_USER", "blackadmin");
+define("DB_PASS", "@#DSDW@#E##@");
+define("DB_NAME", "blackdb");
+define("ADMIN_EMAIL", "Admin Email");
+define("APP_PATH", dirname(__FILE__,2) . "/"); // Don't Change
+define("APP_VERSION", "v3.5.1"); // Don't Change
